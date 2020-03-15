78bd27527b1820c5f07ef8ccb3284e3610d3ac41
wfus <wfu@college.harvard.edu>
2020-03-14 22:10:48 -0700

    [testing] finish the wget from reddit to test live

--- testing/get_recent_posts.sh
+++ testing/get_recent_posts.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-wget http://www.reddit.com/r/AmItheAsshole/new/.json?count=10 -O data/aita_recent.jsonl
+wget -qO- http://www.reddit.com/r/AmItheAsshole/new/.json?count=5 \
+| jq '.data.children[]|{title:.data.title,selftext:.data.selftext}' -c \
+> data/aita_recent.jsonl
--- testing/predict_test.sh
+++ testing/predict_test.sh
@@ -4,4 +4,4 @@ allennlp predict \
 --include-package aita.AITAPredictor \
 --include-package aita.test_reader \
 --predictor aita_predictor \
-$1 data/aita_prediction_example.jsonl
+$1 $2
