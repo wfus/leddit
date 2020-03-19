1778d823f1e884fced52bf08c72528daca78cac5
wfus <wfu@college.harvard.edu>
2020-03-19 14:24:36 -0700

    [viz] add new examples

--- visualization/attention/src/ModelInput.jsx
+++ visualization/attention/src/ModelInput.jsx
@@ -9,17 +9,17 @@ import ModelIntro from './components/ModelIntro'
 
 const examples = [
   {
-    long_text_input: "long text input for example 1",
-    short_text_input: "short text input for example 1"
+    short_text_input: "AITA for being a Navy Seal?",
+    long_text_input: "What the fuck did you just fucking say about me, you little bitch? I'll have you know I graduated top of my class in the Navy Seals, and I've been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I'm the top sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You're fucking dead, kid.",
   },
   {
-    long_text_input: "long text input for example 2",
-    short_text_input: "short text input for example 2"
+    short_text_input: "AITA for telling my SO that she's chubby?",
+    long_text_input: "For context, my SO is getting a bit chubby. She eats 6 meals a week, and her BMI is getting awfully close to 25. I'm worried for her health, but she refuses to go to the gym. AITA if I tell her she's fat to convince her to go to the local gym (Gold's Gym)?",
   },
   {
-    long_text_input: "long text input for example 3",
-    short_text_input: "short text input for example 3"
-  }
+    short_text_input: "AITA for playing basketball outside of park hours?",
+    long_text_input: "Playing basketball at the local school is against the rules after dusk, even though there are lamps and lights there. Am I the asshole for ballin outside of the official hours? The school districit never bothered to change the rules after the lights were installed.",
+  },
 ];
 
 // TODO: This determines what text shows up in the select box for each example.  The input to
@@ -28,10 +28,10 @@ function summarizeExample(example) {
   return example.long_text_input.substring(0, 60);
 }
 
-const title = "AITA Model";
+const title = "AITA Model Visualization";
 const description = (
   <span>
-    Transformer based model for the AITA dataset.
+    Transformer based models for the AITA dataset.
   </span>
 );
 
@@ -84,14 +84,13 @@ class ModelInput extends React.Component {
          * text, like a question (the `input`).  You'll probably want to change the variable names
          * here to match the input variable names in your model.
          */}
-
         <div className="form__field">
-          <label>Long text input</label>
-          <textarea ref={(x) => this.long_text_input = x} type="text" autoFocus="true"></textarea>
+          <label>Post Title</label>
+          <input ref={(x) => this.short_text_input = x} type="text"/>
         </div>
         <div className="form__field">
-          <label>Short text input</label>
-          <input ref={(x) => this.short_text_input = x} type="text"/>
+          <label>Post Body</label>
+          <textarea ref={(x) => this.long_text_input = x} type="text" autoFocus="true"></textarea>
         </div>
 
        {/* You also shouldn't have to change anything below here. */}
