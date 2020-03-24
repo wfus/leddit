64e98b038186a315bf168a0c26bc4c92a5f20f8b
wfus <wfu@college.harvard.edu>
2020-03-23 17:46:20 -0700

    add baseline

--- aita/AITASimpleModel.py
+++ aita/AITASimpleModel.py
@@ -92,3 +92,56 @@ class AITASimpleClassifier(Model):
         return {metric_name: metric.get_metric(reset)
                 for metric_name, metric in self.metrics.items()}
 
+
+@Model.register("lstm_baseline")
+class AITALSTMBaseline(Model):
+    def __init__(self,
+        vocab: Vocabulary,
+        text_field_embedder: TextFieldEmbedder,
+        text_encoder: Seq2VecEncoder,
+        classifier_feedforward: FeedForward,
+        initializer: InitializerApplicator = InitializerApplicator(),
+        regularizer: Optional[RegularizerApplicator] = None):
+        super().__init__(vocab)
+        self.text_field_embedder = text_field_embedder
+        self.text_encoder = text_encoder
+        self.classifier_feedforward = classifier_feedforward
+
+
+    @overrides
+    def forward(self,
+        post: Dict[str, torch.LongTensor],
+        title: Dict[str, torch.LongTensor],
+        label: torch.LongTensor = None) -> Dict[str, torch.Tensor]:
+        post_emb = self.text_field_embedder(post)
+        title_emb = self.text_field_embedder(title)
+        mask_post = util.get_text_field_mask(post)
+        mask_title = util.get_text_field_mask(title)
+        enc_post = self.text_encoder(post_emb, mask_post)
+        enc_title = self.text_encoder(title_emb, mask_title)
+
+        logits = self.classifier_feedforward(
+            torch.cat([enc_post, enc_title], dim=1))
+        output_dict = {"logits": logits}
+
+        if label is not None:
+            loss = self.loss(logits, label)
+            for metric in self.metrics.values():
+                metric(logits, label)
+            output_dict["loss"] = loss
+        
+        return output_dict
+
+    def decode(self, output_dict: Dict[str, torch.Tensor]
+        ) -> Dict[str, torch.Tensor]:
+        """Returns MLE estimate for our classes."""
+        class_probs = F.softmax(output_dict['logits'], dim=-1)
+        output_dict['label'] = class_probs.cpu().numpy().argmax(dim=-1)
+        return output_dict
+
+    @overrides
+    def get_metrics(self, reset: bool = False) -> Dict[str, float]:
+        return {metric_name: metric.get_metric(reset)
+                for metric_name, metric in self.metrics.items()}
+
+
